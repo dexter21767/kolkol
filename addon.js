91617f9ffd2d63da1123db7e6cbdc0862baecd8b
Ahmidi Yasser <88626454+dexter21767@users.noreply.github.com>
2022-10-29 08:26:53 +0100

    Auto-commit

--- addon.js
+++ addon.js
@@ -5,7 +5,31 @@ const {catalog,search,meta,stream} = require("./source");
 const manifest = require("./manifest");
 const builder = new addonBuilder(manifest)
 
+builder.defineStreamHandler((args) => {
+	console.log("addon.js streams:", args);
+	if (args.id.match(/kolkol_id:[^xyz]*/i)) {
+		/*return Promise.resolve(stream(args.type, args.id.split(":")[1],args.id.split(":")[2]))
+		.then((streams) => ({ streams: streams }));*/
+		//.then((streams) => { console.log('streams', streams)});
+	} else {
+		console.log('stream reject');
+		return Promise.resolve({ streams: [] });
+	}
+});
 
+builder.defineMetaHandler((args) => {
+	console.log("addon.js meta:", args);
+
+	if (args.id.match(/kolkol_id:[^xyz]*/i)) {
+		/*return Promise.resolve(meta(args.type, args.id.split(":")[1]))
+			.then((meta) => ({ meta: meta }));*/
+	} else {
+		console.log('meta reject');
+		return Promise.resolve({ meta: [] });
+	}
+
+
+});
 
 builder.defineCatalogHandler((args) => {
 	console.log("addon.js Catalog:", args);
