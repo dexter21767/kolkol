68aa650f00c63924dc5da0437459385572590367
dexter21767 <ahmidiyasser@gmail.com>
2023-03-16 22:14:22 +0100

    Auto-commit

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 63355 ,cacheMaxAge : process.env.cacheMaxAge || 0})
 
 // when you've deployed your addon, un-comment this line
-publishToCentral("https://2ecbbd610840-kolkol.baby-beamup.club/manifest.json")
+publishToCentral("https://2ecbbd610840-kolkol.baby-beamup.club/manifest.json").catch(e=>{console.error(e)})
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
